4e95fbf0b2733bc1254ca5e282501018f6710c71
winters <dannywinter9911@outlook.com>
2024-12-16 09:34:26 +0700

    feature - add api get all articles

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -1,15 +1,28 @@
 import { Router } from "express";
 import {
   addComments,
+  createArticle,
+  getAll,
   getArticleByName,
   updateVote,
 } from "../database/article.js";
 
 const articleRoutes = Router();
 
+articleRoutes.get("/api/articles", async (req, res) => {
+  const articles = await getAll();
+  res.json(articles);
+});
+
 articleRoutes.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
   const article = await getArticleByName(name);
+  res.json(article);
+});
+
+articleRoutes.post("/api/articles", async (req, res) => {
+  const { name, content, title } = req.body;
+  const article = await createArticle({ name, content, title });
   res.json(article);
 });
 
